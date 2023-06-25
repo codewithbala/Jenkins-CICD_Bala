7f4fb5f3826b3f5f5371a6b42ee4a419e6d14083
BalaSudhakar <codewithbala@gmail.com>
2023-06-25 14:29:37 -0400

    change the button text

--- src/main/java/com/mankikdemo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/mankikdemo/tutorial/addressbook/AddressbookUI.java
@@ -38,7 +38,7 @@ public class AddressbookUI extends UI {
      */
     TextField filter = new TextField();
     Grid contactList = new Grid();
-    Button newContact = new Button("Registration");
+    Button newContact = new Button("New Contact");
 
     // ContactForm is an example of a custom component class
     ContactForm contactForm = new ContactForm();
