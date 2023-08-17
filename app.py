b793c9c43c892f34c5713f3cc3617f940af5395c
Gubbala Bala Sudhakar <126678172+codewithbala@users.noreply.github.com>
2023-08-17 08:46:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask inside Docker by Bala!! done on August 17th"
+    return "Flask inside Docker by Bala!! done on August 17th 2023 at 8:46"
 
 
 if __name__ == "__main__":
