8ae8fa491351a6bb8625003ac8328d0788ae78c6
Gubbala Bala Sudhakar <126678172+codewithbala@users.noreply.github.com>
2024-03-21 14:56:55 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! From Varun Manik Simplilearn on Date Apr 09 2023" );
+        System.out.println( "Hello World! From Bala on 2025" );
     }
 }
